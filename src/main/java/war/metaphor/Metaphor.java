49fca05fb84d3b1a8a01162993ecc7e1871246b7
JoyelTheDev <222600183+JoyelTheDev@users.noreply.github.com>
2026-05-22 11:50:13 +0530

    Update Metaphor.java

--- src/main/java/war/metaphor/Metaphor.java
+++ src/main/java/war/metaphor/Metaphor.java
@@ -97,6 +97,7 @@ public class Metaphor {
                 .mutator("watermark", WatermarkMutator.class)
 
                 .mutator("strip", StripMutator.class)
+                .mutator("fake-class", FakeClassTransformer.class)
 
                 .mutator("dot-graph", DotExportMutator.class)
 
