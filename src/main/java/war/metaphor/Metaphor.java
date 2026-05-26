4f2d4418a2d8427d62b029f98adde1b600ba9d23
JoyelTheDev <222600183+JoyelTheDev@users.noreply.github.com>
2026-05-26 11:55:09 +0530

    Update Metaphor.java

--- src/main/java/war/metaphor/Metaphor.java
+++ src/main/java/war/metaphor/Metaphor.java
@@ -84,6 +84,7 @@ public class Metaphor {
                 .mutator("number.salt", SaltingIntegerTransformer.class)
                 .mutator("number.table", IntegerTableTransformer.class)
                 .mutator("mba", MBATransformer.class)
+                .mutator("numberobf", NumberTransformer.class)
                 .mutator("dead-code",  DeadCodeInjectorTransformer.class)
                 .mutator("virtualize",  VirtualizingTransformer.class)  
                 
